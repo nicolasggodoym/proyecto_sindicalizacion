011233bb24fbb13e015e52fddb57c4cf511d8c22
Nicolás Godoy M <nicolas.godoy.m@ug.uchile.cl>
2023-11-19 22:02:37 -0300

    avance revision

--- R/proc_dt_gral.R
+++ R/proc_dt_gral.R
@@ -4,7 +4,7 @@ rm(list=ls())
 
 
 # Cargar paquetes ---------------------------------------------------------
-pacman::p_load(tidyverse, sjmisc)
+pacman::p_load(tidyverse, sjmisc, readxl, data.table)
 
 # Cargar data -------------------------------------------------------------
 files = list.files("input/data/dt", pattern = ".rds")
@@ -13,7 +13,44 @@ data = map(files, ~readRDS(paste0("input/data/dt/", .x)))
 
 names(data) = str_remove(files, pattern = ".rds$")
 
-names(data)
+llave_cae = read_xlsx("input/data/dt/CAE_DT_armonizado.xlsx") %>% 
+  select(id_cae2, ID)
+
+sii = data[["CAE_RUT_FINAL"]]
+
+names(sii)
+
+a=sii %>% 
+  mutate(rut = paste0(rut, "-", dv)) %>% 
+  select(rut, id_cae2) %>% 
+  merge(., llave_cae, all.x = T, by = "id_cae2") %>% 
+  group_by(rut) %>% 
+  summarise(id_cae2 = n_distinct(id_cae2),
+            ID = n_distinct(ID)) %>% 
+  as.data.table()
+
+a = as.data.table()
+
+descr=a[, .(media_id_cae2 = mean(id_cae2),
+          cuartil_25_id_cae2 = quantile(id_cae2, 0.25),
+          cuartil_50_id_cae2 = quantile(id_cae2, 0.5),
+          cuartil_75_id_cae2 = quantile(id_cae2, 0.75),
+          min_id_cae2 = min(id_cae2),
+          max_id_cae2 = max(id_cae2),
+          media_ID = mean(ID),
+          cuartil_25_ID = quantile(ID, 0.25),
+          cuartil_50_ID = quantile(ID, 0.5),
+          cuartil_75_ID = quantile(ID, 0.75),
+          min_ID = min(ID),
+          max_ID = max(ID))]
+
+descr %>% 
+  pivot_longer(everything()) %>% 
+  mutate(stat = str_remove(str_extract(name, pattern = "^.*(?=_)"), "_id"),
+         name = ifelse(str_detect(name, "id_cae2"), "SII", "Armonizada")) %>% 
+  pivot_wider(id_cols = "name",
+              names_from = "stat",
+              values_from = "value")
 
 names(data[["ooss_cen"]])
 
--- R/proc_dt_nc_huelgas.R
+++ R/proc_dt_nc_huelgas.R
@@ -8,9 +8,9 @@ pacman::p_load(tidyverse, readxl, sjmisc)
 # Cargar datos ------------------------------------------------------------
 
 llave = readRDS("input/data/dt/CAE_RUT_FINAL.rds") %>% 
-  select(rut_empresa = rut, dv, cae = id_cae2) %>% 
+  select(rut_empresa = rut, dv, id_cae2) %>% 
   mutate(across(c(rut_empresa, dv), ~as.character(.)),
-         cae = as.numeric(cae))
+         id_cae2 = as.numeric(id_cae2))
   
 
 #Cuadro 1: Listado de instrumentos colectivos y acuerdo de grupo negociador que 
@@ -30,14 +30,35 @@ hoja1 = read_xlsx("input/data/dt/1590_NC_y_Huelgas_2005_2023.xlsx",
          trab_mas_nc = trabajadores_hombres_involucrados,
          trab_fem_nc = trabajadores_mujeres_involucradas,
          trab_tot_nc = trabajadores_total_involucrados) %>% 
-  mutate(rut_empresa = as.character(rut_empresa)) #%>% 
-  # merge(., llave, by = "rut_empresa", all.x = T) %>% 
-  # group_by(rut_empresa, rsu, fecha_inicio_nc) %>% 
-  # mutate(n = row_number()) %>% 
-  # ungroup() %>% 
-  # filter(n==1) %>% 
-  # select(-c(n, rsu))
+  mutate(rut_empresa = as.character(rut_empresa)) %>% 
+  merge(., llave, by = "rut_empresa", all.x = T) #%>%
+  group_by(rut_empresa, rsu, fecha_inicio_nc) %>%
+  mutate(n = row_number()) %>%
+  ungroup() %>%
+  filter(n==1) %>%
+  select(-c(n, rsu))
 
+
+a = merge(hoja1, read_xlsx("input/data/dt/CAE_DT_armonizado.xlsx") %>% 
+            select(id_cae2, ID),
+          by = "id_cae2", all.x = T)  
+
+a %>% 
+  mutate(info = case_when(is.na(id_cae2) & !is.na(cae_dt) ~ "Sólo DT",
+                          !is.na(id_cae2) & is.na(cae_dt) ~ "Sólo SII",
+                          !is.na(id_cae2) & !is.na(cae_dt) ~ "Ambas",
+                          TRUE ~ "Ninguna")) %>% 
+  group_by(info) %>%
+  count %>% 
+  mutate(prop = round(n/nrow(a)*100, 3))
+  
+a %>% group_by(rut_empresa) %>% summarise(n=n()) %>% summarise(dup = sum(n>1),
+                                                               uniq = sum(n==1))
+
+sum(is.na(a$rut_empresa))
+
+
+  
 saveRDS(hoja1, "input/data/dt/neg_col_dt.rds")
 
 #Cuadro 2: Listado de huelgas aprobadas. Desde 01 enero 2005 al 04 octubre 2023
@@ -53,13 +74,31 @@ hoja2 = read_xlsx("input/data/dt/1590_NC_y_Huelgas_2005_2023.xlsx",
          trab_fem_h = trabajadores_mujeres,
          trab_tot_h = trabajadores,
          region_h = region,
-         tipo_org_h = tipo_org_sindical_y_grupo) #%>% 
-  # merge(., llave, by = "rut_empresa", all.x = T) %>% 
-  # group_by(rut_empresa, rsu, fecha_escrutinio_h) %>% 
-  # mutate(n = row_number()) %>% 
-  # ungroup() %>% 
-  # filter(n==1) %>% 
-  # select(-c(n, rsu))
+         tipo_org_h = tipo_org_sindical_y_grupo) %>% 
+  merge(., llave, by = "rut_empresa", all.x = T) #%>%
+  group_by(rut_empresa, rsu, fecha_escrutinio_h) %>%
+  mutate(n = row_number()) %>%
+  ungroup() %>%
+  filter(n==1) %>%
+  select(-c(n, rsu))
+
+a = merge(hoja2, read_xlsx("input/data/dt/CAE_DT_armonizado.xlsx") %>% 
+            select(id_cae2, ID),
+          by = "id_cae2", all.x = T)  
+
+a %>% 
+  mutate(info = case_when(is.na(id_cae2) & !is.na(cae_dt) ~ "Sólo DT",
+                          !is.na(id_cae2) & is.na(cae_dt) ~ "Sólo SII",
+                          !is.na(id_cae2) & !is.na(cae_dt) ~ "Ambas",
+                          TRUE ~ "Ninguna")) %>% 
+  group_by(info) %>%
+  count %>% 
+  mutate(prop = round(n/nrow(a)*100, 3))
+
+a %>% group_by(rut_empresa) %>% summarise(n=n()) %>% summarise(dup = sum(n>1),
+                                                               uniq = sum(n==1))
+
+sum(is.na(a$rut_empresa))
 
 saveRDS(hoja2, "input/data/dt/huelgas_ntrab_dt.rds")
 
@@ -81,13 +120,31 @@ hoja3 = read_xlsx("input/data/dt/1590_NC_y_Huelgas_2005_2023.xlsx",
          trab_fem_h = trabajadores_mujeres,
          trab_tot_h = trabajadores,
          region_h = region) %>% 
-  mutate(rut_empresa = as.character(rut_empresa))# %>% 
-  # merge(., llave, by = "rut_empresa", all.x = T) %>% 
-  # group_by(rut_empresa, rsu, fecha_inicio_h) %>% 
-  # mutate(n = row_number()) %>% 
-  # ungroup() %>% 
-  # filter(n==1) %>% 
-  # select(-c(n, rsu))
+  mutate(rut_empresa = as.character(rut_empresa)) %>% 
+  merge(., llave, by = "rut_empresa", all.x = T)# %>%
+  group_by(rut_empresa, rsu, fecha_inicio_h) %>%
+  mutate(n = row_number()) %>%
+  ungroup() %>%
+  filter(n==1) %>%
+  select(-c(n, rsu))
+  
+  a = merge(hoja3, read_xlsx("input/data/dt/CAE_DT_armonizado.xlsx") %>% 
+              select(id_cae2, ID),
+            by = "id_cae2", all.x = T)  
+  
+  a %>% 
+    mutate(info = case_when(is.na(id_cae2) & !is.na(cae_dt) ~ "Sólo DT",
+                            !is.na(id_cae2) & is.na(cae_dt) ~ "Sólo SII",
+                            !is.na(id_cae2) & !is.na(cae_dt) ~ "Ambas",
+                            TRUE ~ "Ninguna")) %>% 
+    group_by(info) %>%
+    count %>% 
+    mutate(prop = round(n/nrow(a)*100, 3))
+  
+  a %>% group_by(rut_empresa) %>% summarise(n=n()) %>% summarise(dup = sum(n>1),
+                                                                 uniq = sum(n==1))
+  
+  sum(is.na(a$rut_empresa))
 
 saveRDS(hoja3, "input/data/dt/huelgas_dur_dt.rds")
 
